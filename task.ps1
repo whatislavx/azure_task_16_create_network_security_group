a42296fd2b0b52310b569d5ab748d60e1faafc21
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-14 12:21:22 +0200

    Update network security group rules and modify artifacts.json

--- task.ps1
+++ task.ps1
@@ -1,6 +1,5 @@
 $location = "francecentral"
 $resourceGroupName = "mate-azure-task-16"
-
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
 $webSubnetName = "webservers"
@@ -9,7 +8,6 @@ $dbSubnetName = "database"
 $dbSubnetIpRange = "10.20.30.64/26"
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
-
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup `
@@ -20,7 +18,7 @@ $nsg_rule_inbound = New-AzNetworkSecurityRuleConfig `
     -Name all-inbound-rule `
     -Description "Allow all subnets Inbound" `
     -Access Allow `
-    -Protocol Tcp `
+    -Protocol * `
     -Direction Inbound `
     -Priority 150 `
     -SourceAddressPrefix $vnetAddressPrefix `
@@ -32,7 +30,7 @@ $nsg_rule_outbound = New-AzNetworkSecurityRuleConfig `
     -Name all-outbound-rule `
     -Description "Allow all subnets Outbound" `
     -Access Allow `
-    -Protocol Tcp `
+    -Protocol * `
     -Direction Outbound `
     -Priority 150 `
     -SourceAddressPrefix $vnetAddressPrefix `
@@ -40,7 +38,7 @@ $nsg_rule_outbound = New-AzNetworkSecurityRuleConfig `
     -DestinationAddressPrefix $vnetAddressPrefix `
     -DestinationPortRange *
 
-Write-Host "Creating web network security group..."
+Write-Host "Creating webSubnet network security group..."
 $web_nsg_http_https_rule = New-AzNetworkSecurityRuleConfig -Name web-http-https-rule `
     -Description "Allow HTTP and HTTPS" `
     -Access Allow `
@@ -65,7 +63,7 @@ $mng_nsg_rule_ssh = New-AzNetworkSecurityRuleConfig `
     -Access Allow `
     -Protocol Tcp `
     -Direction Inbound `
-    -Priority 102 `
+    -Priority 101 `
     -SourceAddressPrefix * `
     -SourcePortRange * `
     -DestinationAddressPrefix $mngSubnetIpRange `
@@ -78,36 +76,23 @@ $mng_nsg = New-AzNetworkSecurityGroup `
     -SecurityRules $mng_nsg_rule_ssh, $nsg_rule_inbound, $nsg_rule_outbound
 
 Write-Host "Creating dbSubnet network security group..."
-$db_nsg_rule_web = New-AzNetworkSecurityRuleConfig `
+$db_nsg_rule = New-AzNetworkSecurityRuleConfig `
     -Name sql-from-web `
-    -Description "Allow SQL from web" `
+    -Description "Allow SQL from virtual netowrk" `
     -Access Allow `
     -Protocol Tcp `
     -Direction Inbound `
-    -Priority 103 `
-    -SourceAddressPrefix $webSubnetIpRange `
+    -Priority 102 `
+    -SourceAddressPrefix $vnetAddressPrefix `
     -SourcePortRange * `
     -DestinationAddressPrefix $dbSubnetIpRange `
-    -DestinationPortRange 3306
-
-$db_nsg_rule_mng = New-AzNetworkSecurityRuleConfig `
-    -Name sql-from-mng `
-    -Description "Allow SQL from management" `
-    -Access Allow `
-    -Protocol Tcp `
-    -Direction Inbound `
-    -Priority 104 `
-    -SourceAddressPrefix $mngSubnetIpRange `
-    -SourcePortRange * `
-    -DestinationAddressPrefix $dbSubnetIpRange `
-    -DestinationPortRange 3306
+    -DestinationPortRange 5432 # PostgreSQL
 
 $db_nsg = New-AzNetworkSecurityGroup `
     -ResourceGroupName $resourceGroupName `
     -Location $location `
     -Name $dbSubnetName `
-    -SecurityRules $db_nsg_rule_web, $db_nsg_rule_mng
-
+    -SecurityRules $db_nsg_rule
 
 Write-Host "Creating a virtual network ..."
 $webSubnet = New-AzVirtualNetworkSubnetConfig `
